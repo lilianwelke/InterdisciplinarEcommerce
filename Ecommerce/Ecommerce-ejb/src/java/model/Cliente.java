e8e7245056e1e01740769fd8127fa2d5ab28eb0f
Gabrielli <gabrielli.vianna30@hotmail.com>
2018-07-04 20:48:03 -0300

    projeto

---
+++ Ecommerce/Ecommerce-ejb/src/java/model/Cliente.java
@@ -0,0 +1,76 @@
+package model;
+
+public class Cliente {
+    private int ccliente;
+    private String cliente;
+    private String cpf;
+    private String endereco;
+    private String cidade;
+    private int cep;
+    private String uf;
+    private int telefone;
+
+    public int getCcliente() {
+        return ccliente;
+    }
+
+    public void setCcliente(int ccliente) {
+        this.ccliente = ccliente;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getEndereco() {
+        return endereco;
+    }
+
+    public void setEndereco(String endereco) {
+        this.endereco = endereco;
+    }
+
+    public String getCidade() {
+        return cidade;
+    }
+
+    public void setCidade(String cidade) {
+        this.cidade = cidade;
+    }
+
+    public int getCep() {
+        return cep;
+    }
+
+    public void setCep(int cep) {
+        this.cep = cep;
+    }
+
+    public String getUf() {
+        return uf;
+    }
+
+    public void setUf(String uf) {
+        this.uf = uf;
+    }
+
+    public int getTelefone() {
+        return telefone;
+    }
+
+    public void setTelefone(int telefone) {
+        this.telefone = telefone;
+    }
+}
